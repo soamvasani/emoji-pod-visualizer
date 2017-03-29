375b1ead4862371d1db651180bdd5ffe8ba9888c
Soam Vasani <soamvasani@platform9.com>
2017-03-30 00:20:51 +0200

    Move scripts and css to static/
    Add some emoji images

---
+++ static/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// Create a new HTML5 EventSource
+var source = new EventSource('/events/');
+
+// Create a callback for when a new message is received.
+source.onmessage = function(e) {
+    console.log("got: ", e.data);
+    var o = JSON.parse(e.data);
+    handleEvent(o);
+};
+
+
+var ps = [];
+
+function handleEvent(e) {
+    //e.podName
+    //e.containerStates : name->state
+
+    ps[e.podName] = e.containerStates;
+
+    redraw();
+}
+
+const running = "";
+const creating = "";
+const error = "";
+
+function redraw() {
+    var out = "";
+    for (var podName in ps) {
+        //out += JSON.stringify(ps[podName]);
+        //out += "<br/>";
+        var cStates = ps[podName];
+        
+        out += '<div class="pod">';
+        out += '<span class="podName">' + podName + "</span>";
+        for (var cName in cStates) {
+            var state = cStates[cName];
+            if (state === 'running') {
+                out += '<img src="/static/smiley.png">';
+            } else if (state === 'ContainerCreating') {
+                out += '<img src="/static/baby.png">';
+            } else if (state === 'terminated') {
+                out += '<img src="/static/ghost.png">';
+            } else {
+                out += state;
+            }
+        }
+        out += '</div>';
+        
+    }
+    $("#podinfo").html(out);
+}
+
+// // Create a new HTML5 EventSource
+// var source = new EventSource('/events/');
+
+// // Create a callback for when a new message is received.
+// source.onmessage = function(e) {
+//     console.log("got: ", e.data);
+//     var o = JSON.parse(e.data);
+//     handleEvent(o);
+// };
+
+
+// var ps = [];
+
+// function handleEvent(e) {
+//     //e.podName
+//     //e.containerStates : name->state
+
+//     ps[e.podName] = e.containerStates;
+
+//     redraw();
+// }
+
+// function redraw() {
+//     var out = "";
+//     for (var podName in ps) {
+//         out += JSON.stringify(ps[podName]);
+//         out += "<br/>";
+//     }
+//     $("#podinfo").html(out);
+//}
